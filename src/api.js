ed61b1e2d15c2bfbd912764759a0187d3a561ffe
Behuamuh <sven1980@list.ru>
2020-11-04 13:50:28 +0300

    fixed api url protocol & added gh pages

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = 'http://jsonplaceholder.typicode.com';
+const API_URL = 'https://jsonplaceholder.typicode.com';
 
 export const getPosts = async (start, limit = 10) => {
   const url = `${API_URL}/posts?_start=${start}&_limit=${limit}`;
